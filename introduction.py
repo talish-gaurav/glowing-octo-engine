88328da622f97b727a021ff88d342a0920d402d2
talish-gaurav <97028189+talish-gaurav@users.noreply.github.com>
2024-10-22 18:46:31 +0100

    Add files via upload

---
+++ introduction.py
@@ -0,0 +1,15 @@
+a=29
+print("value of a:" ,a)
+print("Data type af a is:", type(a))
+
+b=9.6
+print("value of b:" ,b)
+print("Data type af b is:", type(b))
+
+c=False
+print("value of c:" ,c)
+print("Data type af c is:", type(c))
+
+d="My name is Talish"
+print("value of d:" ,d)
+print("Data type af d is:", type(d))
